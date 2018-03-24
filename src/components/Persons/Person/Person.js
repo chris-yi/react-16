e88cb708073c46d0028317f29a636ccebc2782b1
chris-yi <kri5to.yi@gmail.com>
2018-03-24 12:09:58 -0600

    using refs

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -9,23 +9,30 @@ class Person extends Component {
     constructor(props) {
         super(props);
         console.log(`[Person.js] Inside Contructor`, props)
-      }
-    
-      componentWillMount() {
+    }
+
+    componentWillMount() {
         console.log(`[Person.js] Inside Component Will Mount`)
-      }
-    
-      componentDidMount() {
-        console.log(`[Person.js] Inside Component Did Mount`)
-      }
-    
-      render() {
+    }
+
+    componentDidMount() {
+        console.log(`[Person.js] Inside Component Did Mount`);
+        if (this.props.position === 0) {
+            this.inputElement.focus();
+        }
+    }
+
+    render() {
         console.log(`[Person.js] Inside render()`)
         return (
             <Aux>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <input
+                    ref={(input) => { this.inputElement = input }}
+                    type="text"
+                    onChange={this.props.changed}
+                    value={this.props.name} />
             </Aux>
         )
     }
